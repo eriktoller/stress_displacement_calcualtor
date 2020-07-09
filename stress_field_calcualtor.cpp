1a42cdc4d55f6a9dad47b6b74194d80d718fa1c5
Erik Toller <54261045+eriktoller@users.noreply.github.com>
2020-07-09 14:06:42 +0200

    dummy version
    Just a dummy version to upload

---
+++ stress_field_calcualtor.cpp
@@ -0,0 +1,15 @@
+// This program is under progress and will be upadted
+
+// Initaly some dummy code is included to create the file
+
+#include <iostream>
+#include <cmath>
+
+int main()
+{
+	double number;
+	std::cout << "Input a number: ";
+	std::cin >> number;
+	std::cout << "This is your give number: " << number << std::endl;
+	return 0;
+}
